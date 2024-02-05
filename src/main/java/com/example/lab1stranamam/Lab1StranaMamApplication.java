b283a85e777a199bc35c9f9cd9dea0b690b946f5
Egor Nikitin <egornikitin1401@gmail.com>
2024-02-05 12:43:50 +0300

    init

---
+++ src/main/java/com/example/lab1stranamam/Lab1StranaMamApplication.java
@@ -0,0 +1,13 @@
+package com.example.lab1stranamam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab1StranaMamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab1StranaMamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/lab1stranamam/Lab1StranaMamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab1stranamam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab1StranaMamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
